16209ad6ac52ca0251e566af7919d64801164105
OrgaChem <orga.chem.job@gmail.com>
2013-09-28 02:49:54 +0900

    Support to listen custom event

--- socketio.js
+++ socketio.js
@@ -10,6 +10,7 @@
 goog.provide('socketIo.Socket');
 
 goog.require('goog.Uri');
+goog.require('goog.array');
 goog.require('goog.asserts');
 goog.require('goog.dom');
 goog.require('goog.events.EventHandler');
@@ -80,11 +81,35 @@ socketIo.Socket.imported_ = false;
 
 
 /**
+ * @type {Object.<function>}
+ * @private
+ */
+socketIo.Socket.wrapperMap_ = {};
+
+
+/**
  * SocketNamespace object from Socket.IO.
  * @type {SocketNamespace}
  * @private
  */
 socketIo.Socket.prototype.socket_ = null;
+
+
+/** @override */
+socketIo.Socket.prototype.addEventListener = function(type, handler,
+    opt_capture, opt_handlerScope) {
+
+  var wrapperMap = socketIo.Socket.wrapperMap_;
+  var wrapper;
+
+  if (!(type in wrapperMap)) {
+    wrapper = wrapperMap[type] = this.createWrapper(type)
+    this.addCustomEventListener(type, wrapper)
+  }
+
+  goog.base(this, 'addEventListener', type, handler, opt_capture,
+      opt_handlerScope);
+};
 
 
 /**
@@ -97,18 +122,63 @@ socketIo.Socket.prototype.assertSocketExists_ = function() {
 
 
 /**
- * Adds an event listener to the event target.
- * The mechanism to listen customize events by Socket.IO, so methods from
- * {@link goog.events.EventTarget} do not work well when your listener was
- * attached by the method.
- *
+ * Adds a listener to the end of the listeners array for the specified event.
  * @param {string} type The type of the event to listen for.
- * @param {Function|Object} handler The function to handle the event. The
- *     handler can also be an object that implements the handleEvent method
- *     which takes the event object as argument.
+ * @param {Function|Object} handler The function to handle the event.
+ * @private
  */
-socketIo.Socket.prototype.addCustomEventListener = function(type, handler) {
-  this.socket_['on'](type, goog.bind(handler, this));
+socketIo.Socket.prototype.addCustomEventListener_ = function(type, handler) {
+  this.assertSocketExists_();
+  this.socket_['on'](type, handler);
+};
+
+
+/**
+ * Remove a listener from the listener array for the specified event.
+ * Caution: changes array indices in the listener array behind the listener.
+ * @param {string} type The type of the event to listen for.
+ * @param {Function|Object} handler The function to handle the event.
+ * @private
+ */
+socketIo.Socket.prototype.removeCustomEventListener_ = function(type, handler) {
+  var wrapperMap = socketIo.Socket.wrapperMap_;
+
+  this.assertSocketExists_();
+  this.socket_['removeListener'](type, handler);
+};
+
+
+/**
+ * Returns created listener wrapper for Socket.IO.
+ * @param {string} type The event type of wrapper.
+ * @return {Function} Created wrapper function.
+ * @protected
+ */
+socketIo.Socket.prototype.createWrapper = function(type) {
+  var wrapper;
+  var that = this;
+
+  switch (type) {
+    case socketIo.Socket.EventType.MESSAGE:
+      return function(msg) {
+        that.dispatchEvent({ type: type, message: msg });
+      };
+    case socketIo.Socket.EventType.CONNECT:
+      return function() {
+        that.dispatchEvent({ type: type });
+      };
+    case socketIo.Socket.EventType.DISCONNECT:
+    case socketIo.Socket.EventType.CONNECT_FAILED:
+    case socketIo.Socket.EventType.ERROR:
+      return function(reason) {
+        that.dispatchEvent({ type: type, data: reason });
+      };
+    default:
+      return function() {
+        var args = goog.array.toArray(arguments);
+        that.dispatchEvent({ type: type, data: args });
+      };
+  }
 };
 
 
@@ -136,16 +206,14 @@ socketIo.Socket.prototype.open = function(url) {
 
 /**
  * Imports client-side Socket.IO script.
- *
- * @param {goog.dom.DomHelper=} opt_domHelper Optional DOM helper.
  */
-socketIo.Socket.prototype.importSocketIo = function(opt_domHelper) {
+socketIo.Socket.prototype.importSocketIo = function() {
   if (socketIo.Socket.imported_) {
-    this.handleScriptLoad();
+    this.handleScriptLoad_();
     return;
   }
 
-  var dom = opt_domHelper || goog.dom.getDomHelper();
+  var dom = goog.dom.getDomHelper();
   var uriObj = goog.Uri.parse(this.serverAddr_);
   uriObj.setPath(socketIo.Socket.SCRIPT_PATH);
 
@@ -153,7 +221,7 @@ socketIo.Socket.prototype.importSocketIo = function(opt_domHelper) {
       'type': 'text/javascript' });
 
   this.handler_.listen(script, goog.events.EventType.LOAD,
-      this.handleScriptLoad);
+      this.handleScriptLoad_);
 
   dom.getDocument().body.appendChild(script)
   socketIo.Socket.imported_ = true;
@@ -192,49 +260,10 @@ socketIo.Socket.prototype.dispatchEventOnServer = function(e) {
 
 
 /**
- * Handles Socket.IO connect event.
- * @private
- */
-socketIo.Socket.prototype.handleConnect_ = function() {
-  this.dispatchEvent({ type: socketIo.Socket.EventType.CONNECT });
-};
-
-
-/**
- * Handles Socket.IO connect_failed event.
- * @private
- */
-socketIo.Socket.prototype.handleConnectFailed_ = function(reason) {
-  this.dispatchEvent({ type: socketIo.Socket.EventType.CONNECT_FAILED,
-      reason: reason });
-};
-
-
-/**
- * Handles Socket.IO disconnect event.
- * @private
- */
-socketIo.Socket.prototype.handleDisConnect_ = function(reason) {
-  this.dispatchEvent({ type: socketIo.Socket.EventType.DISCONNECT,
-      reason: reason });
-};
-
-
-/**
- * Handles Socket.IO error event.
- * @private
- */
-socketIo.Socket.prototype.handleError_ = function(reason) {
-  this.dispatchEvent({ type: socketIo.Socket.EventType.ERROR,
-      reason: reason });
-};
-
-
-/**
  * Handles Socket.IO message event.
  * @private
  */
-socketIo.Socket.prototype.handleMessage_ = function(msg) {
+socketIo.Socket.prototype.onmessage_ = function(msg) {
   this.dispatchEvent({ type: socketIo.Socket.EventType.MESSAGE,
       message: msg });
 };
@@ -242,21 +271,14 @@ socketIo.Socket.prototype.handleMessage_ = function(msg) {
 
 /**
  * Handles the event when fired client-side Socket.IO script was loaded.
+ * @private
  */
-socketIo.Socket.prototype.handleScriptLoad = function() {
+socketIo.Socket.prototype.handleScriptLoad_ = function() {
   var io = goog.global['io'];
 
   if (!goog.isDefAndNotNull(io)) {
     throw Error('Cannot find io: ' + io);
   }
-
-  this.socket_ = io['connect'](this.serverAddr_);
-
-  this.addCustomEventListener('connect', this.handleConnect_);
-  this.addCustomEventListener('disconnect', this.handleDisConnect_);
-  this.addCustomEventListener('connect_failed', this.handleConnectFailed_);
-  this.addCustomEventListener('error', this.handleError_);
-  this.addCustomEventListener('message', this.handleMessage_);
 };
 
 
